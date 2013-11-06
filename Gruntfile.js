7176a9775b6f3f0112e00f1b4b834f54d6f707bf
darul <darul75@gmail.com>
2013-11-06 23:05:42 +0100

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['src/ng-prettyjson.js', 'test/**/*.js']
+    },
+    // KARMA TASK CONFIG
+    karma: {
+      unit: {
+        options: {
+          basePath: './',
+          frameworks: ['jasmine'],
+          browsers: ['Chrome'],
+          autoWatch: true,
+          singleRun: true,
+          files: [            
+            'src/bower_components/angular/angular.js',
+            'src/bower_components/angular-mocks/angular-mocks.js',                                    
+            'src/ng-prettyjson.js',
+            'test/**/*Spec.js']
+        }
+      }
+    },
+    // UGLIFY TASK
+    uglify: {
+      task1: {
+         options: {
+            preserveComments: 'some',
+            report: 'min',
+            banner: '/** \n* @license <%= pkg.name %> - v<%= pkg.version %>\n' + 
+             '* (c) 2013 Julien VALERY http://\n' +
+             '* License: MIT \n**/\n'
+         },         
+         files: {
+             'dist/ng-prettyjson.min.js': ['src/ng-prettyjson.js']
+         }
+       }
+     },
+     // MINIFY CSS
+    cssmin: {
+      options: {
+        keepSpecialComments: false,
+        banner: '/** \n* @license <%= pkg.name %> - v<%= pkg.version %>\n' + 
+             '* (c) 2013 Julien VALERY http://\n' +
+             '* License: MIT \n**/\n'
+      },
+      compress: {
+        files: {          
+          'dist/ng-prettyjson.min.css': ['src/ng-prettyjson.css']
+        }
+      }
+  }
+});
+
+  // LOAD PLUGINS
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-karma');
+
+  // TASK REGISTER
+  grunt.registerTask('default', ['jshint', 'cssmin', 'uglify:task1', 'karma']);
+};
---
+++ test/prettySpec.js
@@ -0,0 +1,29 @@
+describe('test valid json', function () {
+	var $ngCompile, $ngRootScope;
+
+	beforeEach(module('app'));
+
+	beforeEach(inject(function ($compile, $rootScope) {
+		
+		$ngCompile = $compile;
+		$ngRootScope = $rootScope;
+
+		var obj = {a:1, 'b':'foo', c:[false,null, {d:{e:1.3e5}}]};
+		$ngRootScope.json = { json: obj };
+		$ngRootScope.badjson = function() {};
+
+	}));
+
+	it('creates an instance of pretty', function () {
+		var element = $ngCompile('<pre json="badjson" pretty-json />')($ngRootScope);
+		
+		expect(element.find('span').length).toBe(0);
+	});
+
+	it('creates an instance with default values', function () {
+		var element = $ngCompile('<pre json="json" pretty-json />')($ngRootScope);		
+		$ngRootScope.$apply();
+
+		expect(element.find('span').length).toBe(10);		
+	});
+});
